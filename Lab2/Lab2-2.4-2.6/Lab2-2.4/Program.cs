5e6623ed43e33b9f06addedcdcd86d3c1c51c0cd
bdquoc <dangquocbao137@gmail.com>
2023-11-07 08:18:55 +0700

    submit lab2-2.4-2.

---
+++ Lab2/Lab2-2.4-2.6/Lab2-2.4/Program.cs
@@ -0,0 +1,16 @@
+namespace Lab2_22520098_2._4_2._6
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
